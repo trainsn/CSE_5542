7dacad800322dc6f4d90cc7e508b37f7512de2d6
Neng Shi <shineng@zju.edu.cn>
2019-10-12 15:17:00 -0400

    lab3: finish lighting

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -11,12 +11,22 @@ var shaderProgram;  // the shader program
 var CubeVertexPositionBuffer;
 var CubeVertexColorBuffer;
 var CubeVertexIndexBuffer;
+var CubeVertexNormalBuffer;
 var CylinderVertexPositionBuffer;
 var CylinderVertexColorBuffer;
 var CylinderVertexIndexBuffer;
+var CylinderVertexNormalBuffer;
 var SphereVertexPositionBuffer;
 var SphereVertexColorBuffer;
 var SphereVertexIndexBuffer;
+var SphereVertexNormalBuffer;
+
+var light_pos = [1.0,1.0, 1.0, 1.0]; // light pos in eye space 
+var light_color = [1.0,1.0,1.0,1.0];
+var mat_ambient; 
+var mat_diffuse; 
+var mat_specular; 
+var mat_shininess; 
 
 //////////// Init OpenGL Context etc. ///////////////
 
@@ -43,16 +53,26 @@ function webGLStart() {
 
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+    shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+    gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
     shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
     gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+
+    shaderProgram.light_posUniform = gl.getUniformLocation(shaderProgram, "light_pos");
+    shaderProgram.light_colorUniform = gl.getUniformLocation(shaderProgram, "light_color");
+    shaderProgram.ambient_coefUniform = gl.getUniformLocation(shaderProgram, "ambient_coef");
+    shaderProgram.diffuse_coefUniform = gl.getUniformLocation(shaderProgram, "diffuse_coef");
+    shaderProgram.specular_coefUniform = gl.getUniformLocation(shaderProgram, "specular_coef");
+    shaderProgram.shininess_coefUniform = gl.getUniformLocation(shaderProgram, "mat_shininess");
 
     createBuffer();
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    document.addEventListener('mousedown', onDocumentMouseDown,false);
     document.addEventListener('keydown', onKeyDown, false);
 
     drawScene();
@@ -61,9 +81,7 @@ function webGLStart() {
 
 ///////////////////////////////////////////////////////////
 ///////               Create VBO          /////////////////
-function createCube(size){
-    CubeVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexPositionBuffer);
+function createCube(size){  
     var rad = size / 2; 
     var vertices = [
           rad, rad, -rad,
@@ -75,7 +93,8 @@ function createCube(size){
           -rad, -rad, rad,
           rad, -rad, rad, 
     ];
-    
+    CubeVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexPositionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     CubeVertexPositionBuffer.itemSize = 3;
     CubeVertexPositionBuffer.numItems = 8;
@@ -86,7 +105,22 @@ function createCube(size){
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
     CubeVertexIndexBuffer.itemSize = 1;
     CubeVertexIndexBuffer.numItems = 36;
-    
+
+    var normals = [
+          rad, rad, -rad,
+          -rad, rad, -rad,
+          -rad, -rad, -rad,
+          rad, -rad, -rad, 
+          rad, rad, rad,
+          -rad, rad, rad,
+          -rad, -rad, rad,
+          rad, -rad, rad,
+    ];
+    CubeVertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    CubeVertexNormalBuffer.itemSize = 3;
+    CubeVertexNormalBuffer.numItems = 8;
 
     var colors = [
       1.0, 0.0, 0.0, 1.0,
@@ -109,12 +143,13 @@ function createCylinder(tRad, bRad, height, nSlice = 30, nStack = 1){
     var vertices = [];
     var indices = [];
     var colors = [];
+    var normals = [];
     
     var aStep = Math.PI*2 / nSlice;
     var hStep = height / nStack;
     var rStep = (bRad - tRad) / nStack;
 
-    // vertrices and colors 
+    // vertrices and normals 
     for (var i = 0; i < nSlice; i++){
         var a = i * aStep;
         for (var j = 0; j <= nStack; j++){
@@ -123,6 +158,10 @@ function createCylinder(tRad, bRad, height, nSlice = 30, nStack = 1){
             vertices.push(r * Math.cos(a));
             vertices.push(h);
             vertices.push(r * Math.sin(a));
+
+            normals.push(Math.cos(a));
+            normals.push((tRad-bRad)/height);
+            normals.push(Math.sin(a));
 
             for (var k = 0; k < 3; k++){
               if (i % 3 == k)
@@ -139,6 +178,12 @@ function createCylinder(tRad, bRad, height, nSlice = 30, nStack = 1){
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     CylinderVertexPositionBuffer.itemSize = 3;
     CylinderVertexPositionBuffer.numItems = nSlice * (nStack+1);
+
+    CylinderVertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, CylinderVertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    CylinderVertexNormalBuffer.itemSize = 3;
+    CylinderVertexNormalBuffer.numItems = nSlice * (nStack+1);
 
     CylinderVertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, CylinderVertexColorBuffer);
@@ -171,6 +216,7 @@ function createCylinder(tRad, bRad, height, nSlice = 30, nStack = 1){
 function createSphere(rad, nSlice=20, nStack = 20) {
     var vertices = [];
     var indices = [];
+    var normals = [];
     var colors = [];
 
     var height = 2 * rad;
@@ -179,6 +225,7 @@ function createSphere(rad, nSlice=20, nStack = 20) {
 
     // vertices and colors
     vertices.push(0.0, -rad, 0.0);
+    normals.push(0.0, -1.0, 0.0);
     colors.push(1.0, 0.0, 0.0, 1.0);
     for (var j = 1; j < nStack; j++){
         var h = -Math.PI/2 + hStep*j;
@@ -187,6 +234,10 @@ function createSphere(rad, nSlice=20, nStack = 20) {
             vertices.push(Math.cos(h) * Math.cos(a) * rad);
             vertices.push(Math.sin(h) * rad);
             vertices.push(Math.cos(h) * Math.sin(a) * rad);
+
+            normals.push(Math.cos(h) * Math.cos(a));
+            normals.push(Math.sin(h));
+            normals.push(Math.cos(h) * Math.sin(a));
 
             for (var k = 0; k < 3; k++){
               if (i % 3 == k)
@@ -198,6 +249,7 @@ function createSphere(rad, nSlice=20, nStack = 20) {
         }        
     }
     vertices.push(0.0, rad, 0.0);
+    normals.push(0.0, 1.0, 0.0);
     colors.push(1.0, 0.0, 0.0, 1.0);
 
     SphereVertexPositionBuffer = gl.createBuffer();
@@ -205,6 +257,12 @@ function createSphere(rad, nSlice=20, nStack = 20) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     SphereVertexPositionBuffer.itemSize = 3;
     SphereVertexPositionBuffer.numItems = nSlice * (nStack-1) + 2;
+
+    SphereVertexNormalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, SphereVertexNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    SphereVertexNormalBuffer.itemSize = 3;
+    SphereVertexNormalBuffer.numItems = nSlice * (nStack-1) + 2;
 
     SphereVertexColorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, SphereVertexColorBuffer);
@@ -252,6 +310,7 @@ var vMatrix = mat4.create();
 var mMatrix = mat4.create();
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
+var nMatrix = mat4.create();
 var third_vMatrix = mat4.create();
 var first_vMatrix = mat4.create();
 var front_incre = 0.0;
@@ -284,7 +343,7 @@ var fingerCount = 2;
 
 var angle_step = 3.0;
 var arm1Yangle = 0.0;
-var joint1Xangle = 45.0;
+var joint1Xangle = -45.0;
 var palmYangle = 0.0;
 var joint2Zangle = 0.0;
 var joint2Matrix = mat4.create();
@@ -305,11 +364,15 @@ mat4.perspective(60, 1.0, 0.1, 100, pMatrix);
 function setMatrixUniforms(){
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
     gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
 }
 
 function drawCube(){
     gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexPositionBuffer);    // make the cube current buffer 
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, CubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexNormalBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, CubeVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, CubeVertexColorBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, CubeVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
@@ -324,6 +387,9 @@ function drawCylinder(){
     gl.bindBuffer(gl.ARRAY_BUFFER, CylinderVertexPositionBuffer);    // make the cube current buffer 
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, CylinderVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, CylinderVertexNormalBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, CylinderVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, CylinderVertexColorBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, CylinderVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -337,6 +403,9 @@ function drawSphere(){
     gl.bindBuffer(gl.ARRAY_BUFFER, SphereVertexPositionBuffer);    // make the cube current buffer 
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, SphereVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, SphereVertexNormalBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, SphereVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
     gl.bindBuffer(gl.ARRAY_BUFFER, SphereVertexColorBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, SphereVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
@@ -347,11 +416,22 @@ function drawSphere(){
 
 }
 
+function setMat(){
+    gl.uniform4f(shaderProgram.ambient_coefUniform, mat_ambient[0], mat_ambient[1], mat_ambient[2], mat_ambient[3]);
+    gl.uniform4f(shaderProgram.diffuse_coefUniform, mat_diffuse[0], mat_diffuse[1], mat_diffuse[2], mat_diffuse[3]);
+    gl.uniform4f(shaderProgram.specular_coefUniform, mat_specular[0], mat_specular[1], mat_specular[2], mat_specular[3]);
+    gl.uniform1f(shaderProgram.shininess_coefUniform, mat_shininess[0]);
+}
+
 ///////////////////////////////////////////////////////////////////////
 
 function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viweportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // set up lighting 
+    gl.uniform4f(shaderProgram.light_posUniform, light_pos[0], light_pos[1], light_pos[2], light_pos[3]);
+    gl.uniform4f(shaderProgram.light_colorUniform, light_color[0], light_color[1], light_color[2], light_color[3]);
 
     mat4.identity(mMatrix); 
     // global move
@@ -359,12 +439,20 @@ function drawScene() {
 
     // lower part: car + wheels 
     createCylinder(1, 1, 1);
+    mat_ambient = [0.0, 0.15, 0, 1]; 
+    mat_diffuse= [0.0, 0.2, 0, 1]; 
+    mat_specular = [.5, .5, .5, 1]; 
+    mat_shininess = [50.0]; 
+    setMat();
+
     pushMatrix(mMatrix);
       // car cube
       mat4.translate(mMatrix, [0, -carHeight/2, 0], mMatrix);
       pushMatrix(mMatrix);
         mat4.scale(mMatrix, [carWidth, carHeight, carLength], mMatrix);
         mat4.multiply(vMatrix, mMatrix, mvMatrix);
+        mat4.inverse(mvMatrix, nMatrix);
+        mat4.transpose(nMatrix, nMatrix);
         drawCube();
       mat4.set(popMatrix(), mMatrix); 
 
@@ -379,6 +467,8 @@ function drawScene() {
                 pushMatrix(mMatrix);
                   mat4.scale(mMatrix, [wheelRad, wheelHeight, wheelRad], mMatrix);
                   mat4.multiply(vMatrix, mMatrix, mvMatrix);
+                  mat4.inverse(mvMatrix, nMatrix);
+                  mat4.transpose(nMatrix, nMatrix);
                   drawCylinder();
                 mat4.set(popMatrix(), mMatrix);
               mat4.set(popMatrix(), mMatrix);
@@ -394,6 +484,8 @@ function drawScene() {
                 pushMatrix(mMatrix);
                   mat4.scale(mMatrix, [wheelRad, wheelHeight, wheelRad], mMatrix);
                   mat4.multiply(vMatrix, mMatrix, mvMatrix);
+                  mat4.inverse(mvMatrix, nMatrix);
+                  mat4.transpose(nMatrix, nMatrix);
                   drawCylinder();
                 mat4.set(popMatrix(), mMatrix);
               mat4.set(popMatrix(), mMatrix);
@@ -406,6 +498,8 @@ function drawScene() {
     pushMatrix(mMatrix);
       mat4.scale(mMatrix, [baseRad, baseHeight, baseRad], mMatrix);
       mat4.multiply(vMatrix, mMatrix, mvMatrix);
+      mat4.inverse(mvMatrix, nMatrix);
+      mat4.transpose(nMatrix, nMatrix);
       drawCylinder();
     mat4.set(popMatrix(), mMatrix);
 
@@ -415,6 +509,8 @@ function drawScene() {
     pushMatrix(mMatrix);
       mat4.scale(mMatrix, [arm1Width, arm1Height, arm1Width], mMatrix);
       mat4.multiply(vMatrix, mMatrix, mvMatrix);
+      mat4.inverse(mvMatrix, nMatrix);
+      mat4.transpose(nMatrix, nMatrix);
       drawCube();
     mat4.set(popMatrix(), mMatrix);
 
@@ -424,6 +520,8 @@ function drawScene() {
     pushMatrix(mMatrix);
       mat4.scale(mMatrix, [joint1Rad, joint1Rad, joint1Rad], mMatrix);
       mat4.multiply(vMatrix, mMatrix, mvMatrix);
+      mat4.inverse(mvMatrix, nMatrix);
+      mat4.transpose(nMatrix, nMatrix);
       drawSphere();
     mat4.set(popMatrix(), mMatrix); 
 
@@ -432,6 +530,8 @@ function drawScene() {
     pushMatrix(mMatrix);
       mat4.scale(mMatrix, [arm2Width, arm2Height, arm2Width], mMatrix);
       mat4.multiply(vMatrix, mMatrix, mvMatrix);
+      mat4.inverse(mvMatrix, nMatrix);
+      mat4.transpose(nMatrix, nMatrix);
       drawCube();
     mat4.set(popMatrix(), mMatrix);
 
@@ -444,6 +544,8 @@ function drawScene() {
     pushMatrix(mMatrix);
       mat4.scale(mMatrix, [palmLength, palmHeight, palmWidth], mMatrix);
       mat4.multiply(vMatrix, mMatrix, mvMatrix);
+      mat4.inverse(mvMatrix, nMatrix);
+      mat4.transpose(nMatrix, nMatrix);
       drawCube();
     mat4.set(popMatrix(), mMatrix);
     
@@ -459,6 +561,8 @@ function drawScene() {
           pushMatrix(mMatrix);
             mat4.scale(mMatrix, [joint2Rad, joint2Rad, joint2Rad], mMatrix);
             mat4.multiply(vMatrix, mMatrix, mvMatrix);
+            mat4.inverse(mvMatrix, nMatrix);
+            mat4.transpose(nMatrix, nMatrix);
             drawSphere();            
           mat4.set(popMatrix(), mMatrix);
           //finger - cone 
@@ -466,6 +570,8 @@ function drawScene() {
           pushMatrix(mMatrix);
             mat4.scale(mMatrix, [fingerRad, fingerHeight, fingerRad], mMatrix);
             mat4.multiply(vMatrix, mMatrix, mvMatrix);
+            mat4.inverse(mvMatrix, nMatrix);
+            mat4.transpose(nMatrix, nMatrix);
             drawCylinder();
           mat4.set(popMatrix(), mMatrix);  
         mat4.set(popMatrix(), mMatrix);
@@ -482,33 +588,6 @@ function pushMatrix(m){
 function popMatrix(){
     return matrixStack.pop();
 }
-
-///////////////////////////////////////////////////////////////
-//   Below are mouse and key event handlers 
-//
-var lastMouseX = 0, lastMouseY = 0;
-
-///////////////////////////////////////////////////////////////
-
-  function onDocumentMouseDown( event ) {
-
-}
-////////////////////////////////////////////////////////////////////////////////////
-//
-//   Mouse button handlers 
-//
-     function onDocumentMouseMove( event ) {
-
-     }
-
-     function onDocumentMouseUp( event ) {
- 
-     }
-
-     function onDocumentMouseOut( event ) {
-
-     }
-
 
 ///////////////////////////////////////////////////////////////////////////
 //
